18ec6c2a260719eb5ae6693ca9d320c2c7147290
drashti <stech3.silvertech@gmail.com>
2024-09-02 17:46:43 +0530

    chenges-drashti

--- src/Pages/MainPage/Jaherat.jsx
+++ src/Pages/MainPage/Jaherat.jsx
@@ -1,18 +1,70 @@
-import React from 'react'
+// import React from 'react'
+import React, { useState } from "react";
 import Header from '../../Components/Header'
 import background from '../../Images/background.webp'
 import jaherat from '../../Images/jaherat.webp'
 import jaherat2 from '../../Images/jaherat2.webp'
 import Footer from '../../Components/Footer';
-import { SlPlus } from "react-icons/sl";
+import { SlPlus } from "react-icons/sl"
 
 export default function Jaherat() {
+    const [show, setShow] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (image) => {
+    setSelectedImage(image);
+    setShow(true);
+  };
     return (
         <>
+
+        
             {/* // - ------------------------- Header Start -------------------------- */}
             <Header />
             {/* -------------------------- Header end -------------------------- */}
 
+
+
+            <div className='container-fluid' >
+                <div className="row d-block">
+                    <div className="parallax" style={{ backgroundImage: `url(${background})`, position: 'relative', height: '30vh', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed', }}>
+                        <div className="overlay" style={{ position: 'absolute', zIndex: '1', }}>
+
+                        </div>
+                    </div>
+                </div>
+
+            
+          <div className="container p-5">
+            <div className="row pt-5 ">
+                <div className="col-4 image-container">
+                    <div className="image-container">
+                    <img src={jaherat} alt="jaherat" className="w-100 align-content-end" />
+                    <div className="overlay-1">
+                                            <div className="text">
+                                                <SlPlus className='fs-1 fw-bold'/>
+                                            </div>
+                                        </div>
+                                        </div>
+                </div>
+                <div className="col-4 bg-">
+                <div className="image-container">
+                <img src={jaherat2} alt="jaherat" className="w-100 align-content-end" />
+                             <div className="overlay-1">
+                                            <div className="text">
+                                                <SlPlus className='fs-1 fw-bold'/>
+                                            </div>
+                                        </div>
+                                        </div>
+                </div>
+            </div>
+          </div>
+
+
+</div>
+
+{/* 
             <div className='container-fluid' >
                 <div className="row d-block">
                     <div className="parallax" style={{ backgroundImage: `url(${background})`, position: 'relative', height: '30vh', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed', }}>
@@ -27,7 +79,6 @@ export default function Jaherat() {
                     <div className="container">
                         <div className="row">
                             <div className="col-6">
-                                {/* <div className="row mt-5"> */}
                                 <div className='justify-content-end'>
                                     <div className="image-container">
                                         <img
@@ -42,7 +93,6 @@ export default function Jaherat() {
                                         </div>
                                     </div>
                                 </div>
-                                {/* </div> */}
                             </div>
 
                             <div className="col-6">
@@ -65,7 +115,7 @@ export default function Jaherat() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
             {/* --------------------------------Footer Section Start ----------------------------------------*/}
             <Footer />
